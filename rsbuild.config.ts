9e77b2ba2cdf19b915ec3f58b424f56001f01d3a
DcolorWei <corfer@yeah.net>
2025-11-02 09:01:20 +0800

    update config

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,19 +1,27 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
-  plugins: [pluginReact()],
-  source: {
-    entry: {
-      index: './client/index.tsx',
+    html: {
+        title: "app",
     },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:61206',
-        changeOrigin: true,
-      },
+    plugins: [pluginReact()],
+    source: {
+        entry: {
+            index: "./client/index.tsx",
+        },
     },
-  },
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://127.0.0.1:61206",
+                changeOrigin: true,
+            },
+            "/ws": {
+                target: "http://127.0.0.1:61207",
+                changeOrigin: true,
+                ws: true,
+            },
+        },
+    },
 });
